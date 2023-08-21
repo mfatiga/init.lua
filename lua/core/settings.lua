c1f7ed04ac5d022efb4695dab8ccbce705d2d239
mfatiga <mario.fatiga@gmail.com>
2023-08-21 14:13:47 +0200

    disable netrw

--- lua/core/settings.lua
+++ lua/core/settings.lua
@@ -1,3 +1,9 @@
+-- vim.g.netrw_browse_split = 0
+-- vim.g.netrw_banner = 0
+-- vim.g.netrw_winsize = 25
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
 vim.opt.number = true
 vim.opt.relativenumber = true
 
@@ -23,10 +29,6 @@ vim.opt.scrolloff = 1
 vim.opt.signcolumn = "yes"
 --vim.opt.signcolumn = "number"
 vim.opt.isfname:append("@-@")
-
-vim.g.netrw_browse_split = 0
-vim.g.netrw_banner = 0
-vim.g.netrw_winsize = 25
 
 vim.opt.updatetime = 250
 
