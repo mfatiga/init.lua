dd60e1f5f3afe1f95e74d93d521c594407b25247
mfatiga <mario.fatiga@gmail.com>
2023-12-05 11:04:15 +0100

    treesitter enable auto-indent

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -25,5 +25,8 @@ require('nvim-treesitter.configs').setup {
     -- Instead of true it can also be a list of languages
     additional_vim_regex_highlighting = false,
   },
+
+  -- enable auto indentation
+  indent = { enable = true },
 }
 
