f9a8213a2b80d81c867fc8e311810e1cd80346cf
mfatiga <mario.fatiga@gmail.com>
2024-08-23 11:59:34 +0200

    lualine: force refresh when entering or exiting recording mode

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,3 +1,22 @@
+vim.api.nvim_create_autocmd("RecordingEnter", {
+  callback = function()
+    require("lualine").refresh({ place = { "statusline" }, })
+  end,
+})
+
+vim.api.nvim_create_autocmd("RecordingLeave", {
+  callback = function()
+    local timer = vim.loop.new_timer()
+    timer:start(
+      50,
+      0,
+      vim.schedule_wrap(function()
+        require("lualine").refresh({ place = { "statusline" }, })
+      end)
+    )
+  end,
+})
+
 return {
   {
     -- LuaLine
