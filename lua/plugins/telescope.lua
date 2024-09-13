926d88cd2abed93212d053981e08baadffc0de8d
mfatiga <mario.fatiga@gmail.com>
2024-09-13 12:14:19 +0200

    remove duplicate keymap

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -36,7 +36,6 @@ return {
       -- See `:help telescope.builtin`
       local builtin = require 'telescope.builtin'
       vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
-      vim.keymap.set('n', '<leader>?', builtin.oldfiles, { desc = '[?] Find recently opened files' })
       vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
       vim.keymap.set('n', '<leader>sg', builtin.git_files, { desc = '[S]earch [G]it files' })
       vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
