97f76fd2001433fbe1d3050eeb26933947bd4903
mfatiga <mario.fatiga@gmail.com>
2024-09-22 18:26:20 +0200

    formatting

--- lua/plugins/filemgr.lua
+++ lua/plugins/filemgr.lua
@@ -4,12 +4,12 @@ return {
     'nvim-tree/nvim-tree.lua',
     config = function()
       require("nvim-tree").setup({
-          view = {
-            width = 40
-          },
-          update_focused_file = {
-            enable = true
-          },
+        view = {
+          width = 40
+        },
+        update_focused_file = {
+          enable = true
+        },
       })
 
       vim.keymap.set('n', '<leader>tt', require('nvim-tree.api').tree.toggle, { desc = '[T]ree [T]oggle' })
@@ -21,23 +21,23 @@ return {
     'stevearc/oil.nvim',
     config = function()
       require("oil").setup {
-          default_file_explorer = false,
-          columns = { "icon" },
-          keymaps = {
-              ["g?"] = "actions.show_help",
-              ["<CR>"] = "actions.select",
-              ["q"] = "actions.close",
-              ["<C-l>"] = "actions.refresh",
-              ["-"] = "actions.parent",
-              ["_"] = "actions.open_cwd",
-              ["`"] = "actions.cd",
-              ["~"] = "actions.tcd",
-              ["gs"] = "actions.change_sort"
-          },
-          use_default_keymaps = false,
-          view_options = {
-              show_hidden = true,
-          },
+        default_file_explorer = false,
+        columns = { "icon" },
+        keymaps = {
+          ["g?"] = "actions.show_help",
+          ["<CR>"] = "actions.select",
+          ["q"] = "actions.close",
+          ["<C-l>"] = "actions.refresh",
+          ["-"] = "actions.parent",
+          ["_"] = "actions.open_cwd",
+          ["`"] = "actions.cd",
+          ["~"] = "actions.tcd",
+          ["gs"] = "actions.change_sort"
+        },
+        use_default_keymaps = false,
+        view_options = {
+          show_hidden = true,
+        },
       }
 
       -- Open parent directory in current window
