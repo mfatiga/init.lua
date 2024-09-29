eb3163f8c26797d885e171e4779c95c0a37e9bf7
mfatiga <mario.fatiga@gmail.com>
2024-09-29 17:42:38 +0200

    oil.nvim - use opts lazy syntax

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -1,25 +1,26 @@
 return {
   'stevearc/oil.nvim',
-  config = function()
-    require("oil").setup {
-      default_file_explorer = false,
-      columns = { "icon" },
-      keymaps = {
-        ["g?"] = "actions.show_help",
-        ["<CR>"] = "actions.select",
-        ["q"] = "actions.close",
-        ["<C-l>"] = "actions.refresh",
-        ["-"] = "actions.parent",
-        ["_"] = "actions.open_cwd",
-        ["`"] = "actions.cd",
-        ["~"] = "actions.tcd",
-        ["gs"] = "actions.change_sort"
-      },
-      use_default_keymaps = false,
-      view_options = {
-        show_hidden = true,
-      },
-    }
+  opts = {
+    default_file_explorer = false,
+    columns = { "icon" },
+    keymaps = {
+      ["g?"] = "actions.show_help",
+      ["<CR>"] = "actions.select",
+      ["q"] = "actions.close",
+      ["<C-l>"] = "actions.refresh",
+      ["-"] = "actions.parent",
+      ["_"] = "actions.open_cwd",
+      ["`"] = "actions.cd",
+      ["~"] = "actions.tcd",
+      ["gs"] = "actions.change_sort"
+    },
+    use_default_keymaps = false,
+    view_options = {
+      show_hidden = true,
+    },
+  },
+  config = function(_, opts)
+    require("oil").setup(opts)
 
     -- Open parent directory in current window
     vim.keymap.set("n", "<leader>-", "<CMD>Oil<CR>", { desc = "File manager" })
