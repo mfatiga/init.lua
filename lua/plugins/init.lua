f8a654b5de6029f8490f5cabd40f610b4bb82b72
mfatiga <mario.fatiga@gmail.com>
2024-09-29 17:08:37 +0200

    move mini.icons to a separate file

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -2,28 +2,11 @@ return {
   -- Library used by other plugins
   { "nvim-lua/plenary.nvim", lazy = true },
 
-  -- Icons
-  {
-    'echasnovski/mini.icons',
-    opts = {},
-    lazy = false, -- make sure to load on startup
-    priority=1000, -- make sure to load immediately
-    specs = {
-      { "nvim-tree/nvim-web-devicons", enabled = false, optional = true },
-    },
-    init = function()
-      package.preload["nvim-web-devicons"] = function()
-        require("mini.icons").mock_nvim_web_devicons()
-        return package.loaded["nvim-web-devicons"]
-      end
-    end,
-  },
-
   -- Theme
   {
     'sainnhe/gruvbox-material',
     lazy = false, -- make sure we load on startup
-    priority = 999, -- make sure to load immediately
+    priority = 1000, -- make sure to load immediately
     config = function()
       vim.opt.termguicolors = true
       vim.o.background = "dark"
