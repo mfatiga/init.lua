68c367691ad65d3fdd02da21c8cc8eb6b043ea47
mfatiga <mario.fatiga@gmail.com>
2024-10-13 18:04:07 +0200

    lazy load nvim-tree only when a dir is not opened, as it hijacks netrw

--- lua/plugins/nvim_tree.lua
+++ lua/plugins/nvim_tree.lua
@@ -1,7 +1,17 @@
+local function is_dir_opened()
+  local i = 0
+  while i < vim.fn.argc(-1) do
+    local path = vim.fn.argv(i, -1)
+    if vim.fn.isdirectory(path) then return true end
+  end
+  return false
+end
+
 return {
   -- nvim-tree
   'nvim-tree/nvim-tree.lua',
   version = "*",
+  lazy = not is_dir_opened(),
   cmd = { "NvimTreeToggle", "NvimTreeFocus", "NvimTreeClose" },
   keys = {
     { "<leader>tt", "<cmd>NvimTreeToggle<CR>", desc = '[T]ree [T]oggle' },
