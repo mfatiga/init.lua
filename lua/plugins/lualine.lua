27dfc07b9094d3cdae3880d6bf57d9aea94bfe0c
mfatiga <mario.fatiga@gmail.com>
2024-11-06 20:54:59 +0100

    lualine: add plugin updates status

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -20,82 +20,91 @@ vim.api.nvim_create_autocmd("RecordingLeave", {
 return {
   -- LuaLine
   'nvim-lualine/lualine.nvim',
-  opts = {
-    options = {
-      globalstatus = true,
-      icons_enabled = true,
-      theme = LeetVim.theme.extras.lualine,
-      disabled_filetypes = { "ministarter", "alpha" }
-    },
-    extensions = {
-      'lazy', 'mason', 'nvim-tree', 'oil', 'quickfix'
-    },
-    sections = {
-      lualine_a = {
-        'mode',
-        {
-          function()
-            local reg = vim.fn.reg_recording()
-            if reg == "" then return "" end
-            return "REC @" .. reg
-          end
-        }
+  config = function ()
+    local lualine = require('lualine')
+    local lazy_status = require('lazy.status')
+
+    lualine.setup({
+      options = {
+        globalstatus = true,
+        icons_enabled = true,
+        theme = LeetVim.theme.extras.lualine,
+        disabled_filetypes = { "ministarter", "alpha" }
       },
-      lualine_b = {
-        'branch',
-        {
-          'diff',
-          symbols = {
-            added    = " ",
-            modified = " ",
-            removed  = " ",
+      extensions = {
+        'lazy', 'mason', 'nvim-tree', 'oil', 'quickfix'
+      },
+      sections = {
+        lualine_a = {
+          'mode',
+          {
+            function()
+              local reg = vim.fn.reg_recording()
+              if reg == "" then return "" end
+              return "REC @" .. reg
+            end
+          }
+        },
+        lualine_b = {
+          'branch',
+          {
+            'diff',
+            symbols = {
+              added    = " ",
+              modified = " ",
+              removed  = " ",
+            },
+          },
+          'diagnostics'
+        },
+        lualine_c = {
+          {
+            'filename',
+            path=1
+          }
+        },
+        lualine_x = {
+          {
+            lazy_status.updates,
+            cond = lazy_status.has_updates,
+          },
+          'encoding',
+          'fileformat',
+        },
+        lualine_y = {
+          {
+            'filetype',
+            colored = true,
+            icon_only = false,
+            icon = { align = 'right' },
+          }
+        },
+        lualine_z = {
+          {
+            'searchcount',
+            maxcount = 999,
+            timeout = 500,
+            icon = { ' ', align = 'left' },
+            padding = { left = 1, right = 1 }
+          },
+          {
+            'progress',
+            padding = { left = 1, right = 1 }
+          },
+          {
+            'location',
+            padding = { left = 1, right = 1 }
           },
         },
-        'diagnostics'
       },
-      lualine_c = {
-        {
-          'filename',
-          path=1
-        }
-      },
-      lualine_x = {
-        'encoding',
-        'fileformat',
-      },
-      lualine_y = {
-        {
-          'filetype',
-          colored = true,
-          icon_only = false,
-          icon = { align = 'right' },
-        }
-      },
-      lualine_z = {
-        {
-          'searchcount',
-          maxcount = 999,
-          timeout = 500,
-          icon = { ' ', align = 'left' },
-          padding = { left = 1, right = 1 }
-        },
-        {
-          'progress',
-          padding = { left = 1, right = 1 }
-        },
-        {
-          'location',
-          padding = { left = 1, right = 1 }
-        },
-      },
-    },
-    tabline = {
-      lualine_a = {'buffers'},
-      lualine_b = {},
-      lualine_c = {},
-      lualine_x = {},
-      lualine_y = {},
-      lualine_z = {'tabs'}
-    }
-  },
+      tabline = {
+        lualine_a = {'buffers'},
+        lualine_b = {},
+        lualine_c = {},
+        lualine_x = {},
+        lualine_y = {},
+        lualine_z = {'tabs'}
+      }
+    })
+  end
 }
