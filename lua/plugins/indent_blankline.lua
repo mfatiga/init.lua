fa8ac8c25b8fcb4984f5e003439947f92ed128b5
mfatiga <mario.fatiga@gmail.com>
2024-11-06 21:19:10 +0100

    indent-blankline: lazy load

--- lua/plugins/indent_blankline.lua
+++ lua/plugins/indent_blankline.lua
@@ -2,6 +2,8 @@ return {
   -- Add indentation guides even on blank lines
   "lukas-reineke/indent-blankline.nvim",
   main = "ibl",
+  lazy = vim.fn.argc(-1) == 0,
+  event = { "BufReadPre", "BufNewFile" },
   ---@module "ibl"
   ---@type ibl.config
   opts = {
