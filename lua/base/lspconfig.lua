ceb63ec7d23515bdc9af1a2d2e6a11f9464a2015
mfatiga <mario.fatiga@gmail.com>
2025-05-22 14:24:11 +0200

    revert virtual diagnostic lines

--- lua/base/lspconfig.lua
+++ lua/base/lspconfig.lua
@@ -1,9 +1,9 @@
 -- Diagnostic config
 vim.diagnostic.config({
-  -- virtual_text = true,
+  virtual_text = true,
   -- virtual_text = { current_line = true },
-  virtual_lines = true,
-  -- virtual_lines = { current_line = true },
+  -- virtual_lines = true,
+  virtual_lines = { current_line = true },
 })
 
 -- Diagnostic keymaps
